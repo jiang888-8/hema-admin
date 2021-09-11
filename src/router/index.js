a5857caca7f0912f11d52a3d0f16f7c0a3db1200
jiangdou <1565133920@qq.com>
2021-09-11 17:11:39 +0800

    首页框架搭建完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '@/views/Home'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/home',
+    component: Home
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Home/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        杭州22期后台管理系统
+      <el-button class="right-btn">退出</el-button>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="rgb(51, 55, 68)"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <span>用户管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="1-1">用户列表</el-menu-item>
+              </el-menu-item-group>
+
+            </el-submenu>
+            <el-submenu index='2'>
+              <template slot="title">
+                <span>权限管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="2-1">角色列表</el-menu-item>
+                <el-menu-item index="2-1">权限列表</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="3" >
+              <template slot="title">
+                <span>商品管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="3-1">商品列表</el-menu-item>
+                <el-menu-item index="3-1">分类参数</el-menu-item>
+                <el-menu-item index="3-1">商品分类</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="4">
+              <template slot="title">
+                <span>订单管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="4-1">订单列表</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="5">
+              <template slot="title">
+                <span>数据统计</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="5-1">数据报表</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <el-main>欢迎登录</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+  created () {
+
+  },
+
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+#app {
+  height: 100%;
+  .el-container {
+    height: 100%;
+    .el-header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      height: 60px;
+      font-size: 25px;
+      color: #fff;
+      background: #373d41;
+      .right-btn {
+        width: 70px;
+        height: 40px;
+      }
+    }
+
+    .el-container {
+      height: 100%;
+
+      .el-aside {
+        background: #333744;
+
+      }
+
+    }
+  }
+}
+</style>
